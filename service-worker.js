73b3201c35a2d655a21f947b0b83ecf77dbc3601
Travis CI User <travis@example.org>
2019-06-26 09:05:26 +0000

    Travis CI Clean Deploy : 8a5d835c0a38556892342929c2e6d5bbde8a4a69

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3761a80d321884d8724425b9420fc763.js"
+  "/precache-manifest.688bb0caada6fd45ea04c90b86f2aa61.js"
 );
 
 self.addEventListener('message', (event) => {
